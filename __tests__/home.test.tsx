191535ea7fbb0459b732925717cf9d125c1c3420
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-08-15 07:57:34 +0000

    testing for elements on screen

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -6,4 +6,23 @@ describe("Home Page - Rendering", () => {
         render(<Home />)
         expect(screen.getByText("Home Page")).toBeInTheDocument()
     })
+
+    it("should have button with text Click me", () => {
+        render(<Home />)
+        expect(screen.getByRole("button", { name: "Click Me" })).toBeInTheDocument()
+    })
+
+    it("should have input field with label Enter your name", () => {
+        render(<Home />)
+        expect(screen.getByLabelText("Enter your name")).toBeInTheDocument()
+    })
+    it("should have input field with label Enter your age", () => {
+        render(<Home />)
+        // using regex expression to match the label text
+        expect(screen.getByLabelText(/age/)).toBeInTheDocument()
+    })
+    it("should have input field with placeholder Search", () => {
+        render(<Home />)
+        expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument()
+    })
 })
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,21 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <button className='bg-white text-black px-8 py-3 rounded-lg'>
+        Click Me
+      </button>
+      <div>
+        <label htmlFor='name' >Enter your name</label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor='age' >Enter your age</label>
+        <input type="number" id="age" />
+      </div>
+      <div>
+        <input placeholder='Search...' type="text" id="age" />
+      </div>
     </main>
   )
 }
